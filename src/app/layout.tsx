6f7a0855babf81b44b5a5e03f6e6029dd07ee750
whosensei <sukunagojo965@gmail.com>
2025-06-29 01:44:23 +0530

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { DarkModeProvider } from "@/utils/DarkModeContext";
 import { personalInfo } from "@/data/portfolio";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: `${personalInfo.displayName}.`,
@@ -19,6 +20,7 @@ export default function RootLayout({
         <DarkModeProvider>
           {children}
         </DarkModeProvider>
+        <Analytics />
       </body>
     </html>
   );
